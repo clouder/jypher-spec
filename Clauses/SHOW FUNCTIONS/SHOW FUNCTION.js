94bb4f5bbf35821298db7af0dc64c05a38ffbf50
Gumaro Melendez <cloudernew@gmail.com>
2022-03-19 07:25:31 -0600

    Clauses/SHOW FUNCTIONS spec template

---
+++ Clauses/SHOW FUNCTIONS/SHOW FUNCTION.js
@@ -0,0 +1,18 @@
+/**
+ * 2. Listing all functions
+ *
+ * SHOW FUNCTIONS
+ */
+
+/**
+ * 3. Listing functions with filtering on output columns
+ *
+ * SHOW BUILT IN FUNCTIONS YIELD name, isBuiltIn
+ * WHERE name STARTS WITH 'a'
+ */
+
+/**
+ * 4. Listing functions with other filtering
+ *
+ * SHOW FUNCTIONS EXECUTABLE BY CURRENT USER YIELD *
+ */
