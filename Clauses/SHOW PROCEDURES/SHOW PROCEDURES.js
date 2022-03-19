ebf4b3e7d94c6e9d01a91be8aa634d02652fce6a
Gumaro Melendez <cloudernew@gmail.com>
2022-03-19 07:27:30 -0600

    Clauses/SHOW PROCEDURES spec template

---
+++ Clauses/SHOW PROCEDURES/SHOW PROCEDURES.js
@@ -0,0 +1,20 @@
+/**
+ * 2. Listing all procedures
+ *
+ * SHOW PROCEDURES
+ */
+
+/**
+ * 3. Listing procedures with filtering on output columns
+ *
+ * SHOW PROCEDURES YIELD name, admin
+ * WHERE admin
+ */
+
+/**
+ * 4. Listing procedures with other filtering
+ *
+ * SHOW PROCEDURES EXECUTABLE BY CURRENT USER YIELD *
+ *
+ * SHOW PROCEDURES EXECUTABLE BY jake
+ */
