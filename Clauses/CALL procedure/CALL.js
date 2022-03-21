c856022d32ae5cb295bcf415e9d06453cba0d2ba
Gumaro Melendez <cloudernew@gmail.com>
2022-03-21 00:37:30 -0600

    Define CALL procedure spec

--- Clauses/CALL procedure/CALL.js
+++ Clauses/CALL procedure/CALL.js
@@ -41,12 +41,29 @@
  *
  * CALL dbms.security.createUser('example_username', 'example_password')
  */
+[
+    { clause: {
+        name: "CALL"
+        ,procedure: "dbms.security.createUser"
+        ,arguments: [
+            { literal: "example_username" }
+            ,{ literal: "example_password" }
+        ]
+    } }
+]
 
 /**
  * 9. Call a procedure using CALL YIELD *
  *
  * CALL db.labels() YIELD *
  */
+[
+    { clause: {
+        name: "CALL"
+        ,procedure: "db.labels"
+        ,yield: { expression: "*" }
+    } }
+]
 
 /**
  * 10. Call a procedure within a complex query using CALL YIELD
@@ -54,6 +71,26 @@
  * CALL db.labels() YIELD label
  * RETURN count(label) AS numLabels
  */
+[
+    { clause: {
+        name: "CALL"
+        ,procedure: "db.labels"
+        ,yield: { expression: "label" }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: { operator: {
+            name: "AS"
+            ,expressions: [
+                { function: {
+                    name: "count"
+                    ,argument: { expression: "label" }
+                } }
+                ,{ expression: "numLabels" }
+            ]
+        } }
+    } }
+]
 
 /**
  * 11. Call a procedure and filter its results
@@ -62,6 +99,33 @@
  * WHERE label CONTAINS 'User'
  * RETURN count(label) AS numLabels
  */
+[
+    { clause: {
+        name: "CALL"
+        ,procedure: "db.labels"
+        ,yield: { expression: "label" }
+    } }
+    ,{ clause: {
+        name: "WHERE"
+        ,expression: { operator: {
+            name: "CONTAINS"
+            ,expressions: ["label", { literal: "User" }]
+        } }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: { operator: {
+            name: "AS"
+            ,expressions: [
+                { function: {
+                    name: "count"
+                    ,argument: { expression: "label" }
+                } }
+                ,{ expression: "numLabels"}
+            ]
+        } }
+    } }
+]
 
 /**
  * 12. Call a procedure within a complex query and rename its outputs
@@ -71,3 +135,42 @@
  * WHERE n[prop] IS NOT NULL
  * RETURN prop, count(n) AS numNodes
  */
+[
+    { clause: {
+        name: "CALL"
+        ,procedure: "db.propertyKeys"
+        ,yield: { operator: {
+            name: "AS"
+            ,expressions: ["propertyKey", "prop"]
+        } }
+    } }
+    ,{ clause: {
+        name: "MATCH"
+        ,expression: { pattern: [{ node: { name: "n" } }] }
+    } }
+    ,{ clause: {
+        name: "WHERE"
+        ,expression: { operator: {
+            name: "IS NOT NULL"
+            ,expression: { operator: {
+                name: "[]"
+                ,expressions: ["n", "prop"]
+            } }
+        } }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expressions: [
+            "prop"
+            ,{ operator: {
+                name: "AS"
+                ,expressions: [
+                    { function: {
+                        name: "count"
+                        ,argument: { expression: "n" }
+                    } }
+                ]
+            } }
+        ]
+    } }
+]
