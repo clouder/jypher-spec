2d1d283e14ae3154ab8e9fdce34002e0916d8885
Gumaro Melendez <cloudernew@gmail.com>
2022-03-20 22:16:10 -0600

    String Functions spec template

---
+++ Functions/String/String Functions.js
@@ -0,0 +1,108 @@
+/**
+ * 1. left()
+ *
+ * RETURN left('hello', 3)
+ */
+
+/**
+ * 2. ltrim()
+ *
+ * RETURN lTrim('   hello')
+ */
+
+/**
+ * 3. replace()
+ *
+ * RETURN replace("hello", "l", "w")
+ */
+
+/**
+ * 4. reverse()
+ *
+ * RETURN reverse('anagram')
+ */
+
+/**
+ * 5. right()
+ *
+ * RETURN right('hello', 3)
+ */
+
+/**
+ * 6. rtrim()
+ *
+ * RETURN rTrim('hello   ')
+ */
+
+/**
+ * 7. split()
+ *
+ * RETURN split('one,two', ',')
+ */
+
+/**
+ * 8. substring()
+ *
+ * RETURN substring('hello', 1, 3), substring('hello', 2)
+ */
+
+/**
+ * 9. toLower()
+ *
+ * RETURN toLower('HELLO')
+ */
+
+/**
+ * 10. toString()
+ *
+ * RETURN
+ *   toString(11.5),
+ *   toString('already a string'),
+ *   toString(true),
+ *   toString(date({year:1984, month:10, day:11})) AS dateString,
+ *   toString(datetime({
+ *     year:1984,
+ *     month:10,
+ *     day:11,
+ *     hour:12,
+ *     minute:31,
+ *     second:14,
+ *     millisecond: 341,
+ *     timezone: 'Europe/Stockholm'
+ *   })) AS datetimeString,
+ *   toString(duration({minutes: 12, seconds: -60})) AS durationString
+ */
+
+/**
+ * 11. toStringOrNull()
+ *
+ * RETURN
+ *   toStringOrNull(11.5),
+ *   toStringOrNull('already a string'),
+ *   toStringOrNull(true),
+ *   toStringOrNull(date({year:1984, month:10, day:11})) AS dateString,
+ *   toStringOrNull(datetime({
+ *     year:1984,
+ *     month:10,
+ *     day:11,
+ *     hour:12,
+ *     minute:31,
+ *     second:14,
+ *     millisecond: 341,
+ *     timezone: 'Europe/Stockholm'
+ *   })) AS datetimeString,
+ *   toStringOrNull(duration({minutes: 12, seconds: -60})) AS durationString,
+ *   toStringOrNull(['A', 'B', 'C']) AS list
+ */
+
+/**
+ * 12. toUpper()
+ *
+ * RETURN toUpper('hello')
+ */
+
+/**
+ * 13. trim()
+ *
+ * RETURN trim('   hello   ')
+ */
