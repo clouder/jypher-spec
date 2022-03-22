fa7dfde68dc78eb7916eebde7741cc7dde4cdac9
Gumaro Melendez <cloudernew@gmail.com>
2022-03-22 09:15:57 -0600

    Create nodes sepc defs

--- Clauses/CREATE/1 Create nodes.js
+++ Clauses/CREATE/1 Create nodes.js
@@ -3,24 +3,57 @@
  *
  * CREATE (n)
  */
+[
+    { clause: {
+        name: "CREATE"
+        ,expression: { pattern: [
+            { node: { name: "n" } }
+        ] }
+    } }
+]
 
 /**
  * 1.2. Create multiple nodes
  *
  * CREATE (n), (m)
  */
+[
+    { clause: {
+        name: "CREATE"
+        ,expressions: [
+            { pattern: { node: { name: "n" } } }
+            ,{ pattern: { node: { name: "m" } } }
+        ]
+    } }
+]
 
 /**
  * 1.3. Create a node with a label
  *
  * CREATE (n:Person)
  */
+[
+    { clause: {
+        name: "CREATE"
+        ,expression: { pattern: [
+            { node: { name: "n" ,label: "Person" } }
+        ] }
+    } }
+]
 
 /**
  * 1.4. Create a node with multiple labels
  *
  * CREATE (n:Person:Swedish)
  */
+[
+    { clause: {
+        name: "CREATE"
+        ,expression: { pattern: [
+            { node: { name: "n" ,labels: ["Person" ,"Swedish"] } }
+        ] }
+    } }
+]
 
 /**
  * 1.5. Create node and add labels and properties
@@ -34,3 +67,18 @@
  * CREATE (a {name: 'Andy'})
  * RETURN a.name
  */
+[
+    { clause: {
+        name: "CREATE"
+        ,expression: { pattern: [
+            { node: { name: "a" ,properties: { name: { literal: "Andy" } } } }
+        ] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: { operator: {
+            name: "."
+            ,expressions: ["a" ,"name"]
+        } }
+    } }
+]
