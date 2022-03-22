15698649a4568713c832950c8ed6138604327726
Gumaro Melendez <cloudernew@gmail.com>
2022-03-22 09:25:22 -0600

    Create relationships spec defs

--- Clauses/CREATE/2 Create relationships.js
+++ Clauses/CREATE/2 Create relationships.js
@@ -6,6 +6,58 @@
  * CREATE (a)-[r:RELTYPE]->(b)
  * RETURN type(r)
  */
+[
+    { clause: {
+        name: "MATCH"
+        ,expressions: [
+            { pattern: [{ node: { name: "a" ,label: "Person" } }] }
+            ,{ pattern: [{ node: { name: "b" ,label: "Person" } }] }
+        ]
+    } }
+    ,{ clause: {
+        name: "WHERE"
+        ,expression: { operator: {
+            name: "AND"
+            ,expressions: [
+                { operator: {
+                    name: "="
+                    ,expressions: [
+                        { operator: {
+                            name: "."
+                            ,expressions: ["a" ,"name"]
+                        } }
+                        ,{ literal: "A" }
+                    ]
+                } }
+                ,{ operator: {
+                    name: "="
+                    ,expressions: [
+                        { operator: {
+                            name: "."
+                            ,expressions: ["b" ,"name"]
+                        } }
+                        ,{ literal: "B" }
+                    ]
+                } }
+            ]
+        }}
+    } }
+    ,{ clause: {
+        name: "CREATE"
+        ,expression: { pattern: [
+            { node: { name: "a" } }
+            ,{ edge: { name: "r" ,label: "RELTYPE" ,direction: ">" } }
+            ,{ node: { name: "b" } }
+        ] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: { function: {
+            name: "type"
+            ,argument: { expression: "r" }
+        } }
+    } }
+]
 
 /**
  * 2.2. Create a relationship and set properties
@@ -15,3 +67,75 @@
  * CREATE (a)-[r:RELTYPE {name: a.name + '<->' + b.name}]->(b)
  * RETURN type(r), r.name
  */
+ [
+    { clause: {
+        name: "MATCH"
+        ,expressions: [
+            { pattern: [{ node: { name: "a" ,label: "Person" } }] }
+            ,{ pattern: [{ node: { name: "b" ,label: "Person" } }] }
+        ]
+    } }
+    ,{ clause: {
+        name: "WHERE"
+        ,expression: { operator: {
+            name: "AND"
+            ,expressions: [
+                { operator: {
+                    name: "="
+                    ,expressions: [
+                        { operator: {
+                            name: "."
+                            ,expressions: ["a" ,"name"]
+                        } }
+                        ,{ literal: "A" }
+                    ]
+                } }
+                ,{ operator: {
+                    name: "="
+                    ,expressions: [
+                        { operator: {
+                            name: "."
+                            ,expressions: ["b" ,"name"]
+                        } }
+                        ,{ literal: "B" }
+                    ]
+                } }
+            ]
+        }}
+    } }
+    ,{ clause: {
+        name: "CREATE"
+        ,expression: { pattern: [
+            { node: { name: "a" } }
+            ,{ edge: {
+                name: "r"
+                ,label: "RELTYPE"
+                ,properties: {
+                    name: { operator: {
+                        name: "+"
+                        ,expressions: [
+                            { operator: {
+                                name: "."
+                                ,expressions: ["a" ,"name"]
+                            } }
+                            ,{ literal: "<->"}
+                            ,{ operator: {
+                                name: "."
+                                ,expressions: ["b" ,"name"]
+                            } }
+                        ]
+                    } }
+                }
+                ,direction: ">"
+            } }
+            ,{ node: { name: "b" } }
+        ] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: { function: {
+            name: "type"
+            ,argument: { expression: "r" }
+        } }
+    } }
+]
