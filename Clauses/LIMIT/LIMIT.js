f7e390cd4fdec966c225c747a7690efc67780651
Gumaro Melendez <cloudernew@gmail.com>
2022-03-23 05:37:48 -0600

    LIMIT clause spec defs

--- Clauses/LIMIT/LIMIT.js
+++ Clauses/LIMIT/LIMIT.js
@@ -6,6 +6,21 @@
  * ORDER BY n.name
  * LIMIT 3
  */
+[
+    { clause: {
+        name: "MATCH"
+        ,expression: { pattern: [{ node: { name: "n" } }] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: { operator: { name: "." ,expressions: ["n" ,"name"] } }
+    } }
+    ,{ clause: {
+        name: "ORDER BY"
+        ,expression: { operator: { name: "." ,expressions: ["n" ,"name"] } }
+    } }
+    ,{ clause: { name: "LIMIT" ,expression: { literal: 1 } } }
+]
 
 /**
  * 3. Using an expression with LIMIT to return a subset of the rows
@@ -15,6 +30,36 @@
  * ORDER BY n.name
  * LIMIT 1 + toInteger(3 * rand())
  */
+[
+    { clause: {
+        name: "MATCH"
+    } }
+    ,{ clause: {
+        name: "RETURN"
+    } }
+    ,{ clause: {
+        name: "ORDER BY"
+    } }
+    ,{ clause: {
+        name: "LIMIT"
+        ,expression: { operator: {
+            name: "+"
+            ,expressions: [
+                { literal: 1 }
+                ,{ function: {
+                    name: "toInteger"
+                    ,argument: { operator: {
+                        name: "*"
+                        ,expressions: [
+                            { literal: 3 }
+                            ,{ function: { name: "rand" } }
+                        ]
+                    } }
+                } }
+            ]
+        } }
+    } }
+]
 
 /**
  * 4. LIMIT will not stop side effects
@@ -22,14 +67,74 @@
  * CREATE (n)
  * RETURN n
  * LIMIT 0
- *
+ */
+[
+    { clause: {
+        name: "CREATE"
+        ,expression: { pattern: [{ node: { name: "n" } }] }
+    } }
+    ,{ clause: { name: "RETURN" ,expression: "n" } }
+    ,{ clause: { name: "LIMIT" ,expression: { literal: 0 } } }
+]
+
+/**
  * MATCH (n {name: 'A'})
  * SET n.age = 60
  * RETURN n
  * LIMIT 0
- *
+ */
+[
+    { clause: {
+        name: "MATCH"
+        ,expression: { pattern: [
+            { node: { name: "n" ,properties: { name: { literal: "A" } } } }
+        ] }
+    } }
+    ,{ clause: {
+        name: "SET"
+        ,expression: { operator: {
+            name: "="
+            ,expressions: [
+                { operator: {
+                    name: "."
+                    ,expressions: ["n" ,"age"]
+                } }
+                ,{ literal: 60 }
+            ]
+        } }
+    } }
+    ,{ clause: { name: "RETURN" ,expression: "n" } }
+    ,{ clause: { name: "LIMIT" ,expression: { literal: 0 } } }
+]
+
+/**
  * MATCH (n)
  * WITH n LIMIT 1
  * SET n.locked = true
  * RETURN n
  */
+[
+    { clause: {
+        name: "MATCH"
+        ,expression: { pattern: [{ node: { name: "n" } }] }
+    } }
+    ,{ clause: {
+        name: "WITH"
+        ,expression: "n"
+    } }
+    ,{ clause: { name: "LIMIT" ,expression: { literal: 1 } } }
+    ,{ clause: {
+        name: "SET"
+        ,expression: { operator: {
+            name: "="
+            ,expressions: [
+                { operator: {
+                    name: "."
+                    ,expressions: ["n" ,"locked"]
+                } }
+                ,{ literal: true }
+            ]
+        } }
+    } }
+    ,{ clause: { name: "RETURN" ,expression: "n" } }
+]
