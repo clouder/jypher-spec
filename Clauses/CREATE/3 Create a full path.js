4cc1f9192473e6aaa8574059b78c5a8bce58ca7c
Gumaro Melendez <cloudernew@gmail.com>
2022-03-22 19:33:13 -0600

    Create full path spec def

--- Clauses/CREATE/3 Create a full path.js
+++ Clauses/CREATE/3 Create a full path.js
@@ -4,3 +4,22 @@
  * CREATE p = (andy {name:'Andy'})-[:WORKS_AT]->(neo)<-[:WORKS_AT]-(michael {name: 'Michael'})
  * RETURN p
  */
+[
+    { clause: {
+        name: "CREATE"
+        ,expression: { pattern: [
+            { node: {
+                name: "andy"
+                ,properties: { name: { literal: "Andy" } }
+            } }
+            ,{ edge: { type: "WORKS_AT" ,direction: ">" } }
+            ,{ node: { name: "neo" } }
+            ,{ edge: { type: "WORKS_AT" ,direction: "<" } }
+            ,{ node: {
+                name: "michael"
+                ,properties: { name: { literal: "Michael" } }
+            } }
+         ] }
+    } }
+    ,{ clause: { name: "RETURN" ,expression: "p" } }
+]
