be8432bf35d8d54fb1d8de4ddbf7129894508d6c
Gumaro Melendez <cloudernew@gmail.com>
2022-03-22 20:15:41 -0600

    FOREACH spec def

--- Clauses/FOREACH/FOREACH.js
+++ Clauses/FOREACH/FOREACH.js
@@ -5,3 +5,70 @@
  * WHERE start.name = 'A' AND finish.name = 'D'
  * FOREACH (n IN nodes(p) | SET n.marked = true)
  */
+[
+    { clause: {
+        name: "MATCH"
+        ,expression: { operator: {
+            name: "="
+            ,expressions: [
+                "p"
+                ,{ pattern: [
+                    { node: { name: "start" } }
+                    ,{ edge: { direction: ">", length: [1, "inf"] } }
+                    ,{ node: { name: "finish" } }
+                ] }
+            ]
+        } }
+    } }
+    ,{ clause: {
+        name: "WHERE"
+        ,expression: { operator: {
+            name: "AND"
+            ,expressions: [
+                { operator: {
+                    name: "="
+                    ,expressions: [
+                        { operator: {
+                            name: "."
+                            ,expressions: ["start" ,"name"]
+                        } }
+                        ,{ literal: "A" }
+                    ]
+                } }
+                ,{ operator: {
+                    name: "="
+                    ,expressions: [
+                        { operator: {
+                            name: "."
+                            ,expressions: ["finish" ,"name"]
+                        } }
+                        ,{ literal: "D" }
+                    ]
+                } }
+            ]
+        } }
+    } }
+    ,{ clause: {
+        name: "FOREACH"
+        ,list_expression: { operator: {
+            name: "IN"
+            ,expressions: [
+                "n"
+                ,{ function: { name: "nodes" ,argument: { expression: "p" } } }
+            ]
+        }}
+        ,clause: {
+            name: "SET"
+            ,expression: { operator: {
+                name: "="
+                ,expressions: [
+                    { operator: {
+                        name: "."
+                        ,expressions: ["n" ,"marked"]
+                    } }
+                    ,{ literal: true }
+                ]
+            } }
+        }
+    } }
+]
