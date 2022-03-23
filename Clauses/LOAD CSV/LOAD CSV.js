7e8c0862c07cd7711850f59e48fb535ada7b41c7
Gumaro Melendez <cloudernew@gmail.com>
2022-03-23 14:24:36 -0600

    LOAD CSV spec defs incomplete

--- Clauses/LOAD CSV/LOAD CSV.js
+++ Clauses/LOAD CSV/LOAD CSV.js
@@ -4,13 +4,72 @@
  * LOAD CSV FROM 'file:///artists.csv' AS line
  * CREATE (:Artist {name: line[1], year: toInteger(line[2])})
  */
+[
+    { clause: {
+        name: "LOAD CSV"
+        ,from: { literal: "file:///artists.csv"}
+        ,as: { expression: "line" }
+    } }
+    ,{ clause: {
+        name: "CREATE"
+        ,expression: { pattern: [
+            { node: {
+                label: "Artist"
+                ,properties: {
+                    name: { operator: {
+                        name: "[]"
+                        ,expressions: ["line" ,{ literal: 1 }]
+                    } }
+                    ,year: { function: {
+                        name: "toInteger"
+                        ,argument: { operator: {
+                            name: "[]"
+                            ,expressions: ["line" ,{ literal: 2}]
+                        } }
+                    } }
+                }
+            } }
+        ] }
+    } }
+]
 
 /**
  * 4. Import data from a remote CSV file
  *
- * LOAD CSV FROM 'http://data.neo4j.com/bands/artists.csv' AS line CREATE (:Artist {name: line[1],
- *   year: toInteger(line[2])})
+ * LOAD CSV FROM 'http://data.neo4j.com/bands/artists.csv' AS line
+ * CREATE (:Artist {name: line[1], year: toInteger(line[2])})
  */
+[
+    { clause: {
+        name: "LOAD CSV"
+        ,from: { literal: "http://data.neo4j.com/bands/artists.csv" }
+        ,as: { expression: "line" }
+    } }
+    ,{ clause: {
+        name: "CREATE"
+        ,expression: { pattern: [
+            { node: {
+                label: "Artist"
+                ,properties: {
+                    name: { operator: {
+                        name: "[]"
+                        ,expressions: ["line" ,{ literal: 1 }]
+                    } }
+                    ,year: { operator: {
+                        name: "[]"
+                        ,expressions: { function: {
+                            name: "toInteger"
+                            ,argumnet: { operator: {
+                                name: "[]"
+                                ,expressions: ["line" ,{ literal: 2 }]
+                            } }
+                        } }
+                    } }
+                }
+            } }
+        ] }
+    } }
+]
 
 /**
  * 5. Import data from a CSV file containing headers
