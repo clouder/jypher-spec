3874fcb56f3c8f976e1948e972bc1480d58779c5
Gumaro Melendez <cloudernew@gmail.com>
2022-03-22 19:38:26 -0600

    DELETE clause spec defs

--- Clauses/DELETE/DELETE.js
+++ Clauses/DELETE/DELETE.js
@@ -4,6 +4,19 @@
  * MATCH (n:Person {name: 'UNKNOWN'})
  * DELETE n
  */
+[
+    { clause: {
+        name: "MATCH"
+        ,expression: { pattern: [
+            { node: {
+                name: "n"
+                ,label: "Person"
+                ,properties: { name: { literal: "UNKNOWN"} }
+            } }
+        ] }
+    } }
+    ,{ clause: { name: "DELETE" ,expression: "n" } }
+]
 
 /**
  * 3. Delete all nodes and relationships
@@ -11,6 +24,17 @@
  * MATCH (n)
  * DETACH DELETE n
  */
+[
+    { clause: {
+        name: "MATCH"
+        ,expression: { pattern: [{ node: { name: "n"} }] }
+    } }
+    ,{ clause: {
+        name: "DELETE"
+        ,modifier: "DETACH"
+        ,expression: "n"
+    } }
+]
 
 /**
  * 4. Delete a node with all its relationships
@@ -18,6 +42,19 @@
  * MATCH (n {name: 'Andy'})
  * DETACH DELETE n
  */
+[
+    { clause: {
+        name: "MATCH"
+        ,expression: { pattern: [
+            { node: { name: "n" ,properties: { name: { literal: "Andy" } } } }
+        ] }
+    } }
+    ,{ clause: {
+        name: "DELETE"
+        ,modifier: "DETACH"
+        ,expression: "n"
+    } }
+]
 
 /**
  * 5. Delete relationships only
@@ -25,3 +62,17 @@
  * MATCH (n {name: 'Andy'})-[r:KNOWS]->()
  * DELETE r
  */
+[
+    { clause: {
+        name: "MATCH"
+        ,expression: { pattern: [
+            { node: { name: "n" ,properties: { name: { literal: "Andy" } } } }
+            ,{ edge: { name: "r" ,type: "KNOWS" ,direction: ">" } }
+            ,{ node: {} }
+        ] }
+    } }
+    ,{ clause: {
+        name: "DELETE"
+        ,expression: "r"
+    } }
+]
