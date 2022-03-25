4533861338c2afe10164dd19e3153c346450d8e4
Gumaro Melendez <cloudernew@gmail.com>
2022-03-25 17:40:17 -0600

    3 Use ON CREATE and ON MATCH spec defs

--- Clauses/MERGE/3 Use ON CREATE and ON MATCH.js
+++ Clauses/MERGE/3 Use ON CREATE and ON MATCH.js
@@ -6,6 +6,51 @@
  *   SET keanu.created = timestamp()
  * RETURN keanu.name, keanu.created
  */
+[
+    { clause: {
+        name: "MERGE"
+        ,expression: { pattern: [
+            { node: {
+                name: "keanu"
+                ,label: "person"
+                ,properties: {
+                    name: { literal: "Keanu Reeves" }
+                }
+            } }
+        ] }
+        ,modifier: {
+            name: "ON CREATE"
+            ,clause: {
+                name: "SET"
+                ,expression: { operator: {
+                    name: "="
+                    ,expressions: [
+                        { operator: {
+                            name: "."
+                            ,expressions: ["keanu" ,"created"]
+                        } }
+                        ,{ function: {
+                            name: "timestamp"
+                        } }
+                    ]
+                }}
+            }
+        }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expressions: [
+            { operator: {
+                name: "."
+                ,expressions: ["keanu" ,"name"]
+            } }
+            ,{ operator: {
+                name: "."
+                ,expressions: ["keanu" ,"created"]
+            } }
+        ]
+    } }
+]
 
 /**
  * 3.2. Merge with ON MATCH
@@ -15,6 +60,41 @@
  *   SET person.found = true
  * RETURN person.name, person.found
  */
+[
+    { clause: {
+        name: "MERGE"
+        ,expression: { pattern}
+        ,modifier: {
+            name: "ON MATCH"
+            ,clause: {
+                name: "SET"
+                ,expression: { operator: {
+                    name: "="
+                    ,expressions: [
+                        { operator: {
+                            name: "."
+                            ,expressions: ["person" ,"found"]
+                        } }
+                        ,{ literal: true }
+                    ]
+                } }
+            }
+        }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expressions: [
+            { operator: {
+                name: "."
+                ,expressions: ["person" ,"name"]
+            } }
+            ,{ operator: {
+                name: "."
+                ,expressions: ["person" ,"found"]
+            } }
+        ]
+    } }
+]
 
 /**
  * 3.3. Merge with ON CREATE and ON MATCH
@@ -26,6 +106,63 @@
  *   SET keanu.lastSeen = timestamp()
  * RETURN keanu.name, keanu.created, keanu.lastSeen
  */
+[
+    { clause: {
+        name: "MERGE"
+        ,expression: { pattern: [
+            { node: {
+                name: "keanu"
+                ,label: "Person"
+                ,properties: { name: { literal: "Keanu Reeves" } }
+            } }
+        ] }
+        ,modifiers: [
+            { name: "ON CREATE" ,clause: {
+                name: "SET"
+                ,expression: { operator: {
+                    name: "="
+                    ,expressions: [
+                        { operator: {
+                            name: "."
+                            ,expressions: ["keanu" ,"created"]
+                        } }
+                        ,{ function: { name: "timestamp" } }
+                    ]
+                } }
+            } }
+            ,{ name: "ON MATCH" ,clause: {
+                name: "SET"
+                ,expression: { operator: {
+                    name: "="
+                    ,expressions: [
+                        { operator: {
+                            name: "."
+                            ,expressions: ["keanu" ,"lastSeen"]
+                        } }
+                        ,{ function: { name: "timestamp" } }
+                    ]
+                } }
+            } }
+        ]
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expressions: [
+            { operator: {
+                name: "."
+                ,expressions: ["keanu" ,"name"]
+            } }
+            ,{ operator: {
+                name: "."
+                ,expressions: ["keanu" ,"created"]
+            } }
+            ,{ operator: {
+                name: "."
+                ,expressions: ["keanu" ,"lastSean"]
+            } }
+        ]
+    } }
+]
 
 /**
  * 3.4. Merge with ON MATCH setting multiple properties
@@ -37,3 +174,56 @@
  *     person.lastAccessed = timestamp()
  * RETURN person.name, person.found, person.lastAccessed
  */
+[
+    { clause: {
+        name: "MERGE"
+        ,expression: { pattern: [
+            { node: { name: "person" ,label: "Person" } }
+        ] }
+        ,modifier: {
+            name: "ON MATCH"
+            ,clause: {
+                name: "SET"
+                ,expressions: [
+                    { operator: {
+                        name: "="
+                        ,expressions: [
+                            { operator: {
+                                name: "."
+                                ,expressions: ["person" ,"found"]
+                            } }
+                            ,{ literal: true }
+                        ]
+                    } }
+                    ,{ operator: {
+                        name: "="
+                        ,expressions: [
+                            { operator: {
+                                name: "."
+                                ,expressions: ["person" ,"lastAccessed"]
+                            } }
+                            ,{ function: { name: "timestamp" } }
+                        ]
+                    } }
+                ]
+            }
+        }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expressions: [
+            { operator: {
+                name: "."
+                ,expressions: ["person" ,"name"]
+            } }
+            ,{ operator: {
+                name: "."
+                ,expressions: ["person" ,"found"]
+            } }
+            ,{ operator: {
+                name: "."
+                ,expressions: ["person" ,"lastAccessed"]
+            } }
+        ]
+    } }
+]
