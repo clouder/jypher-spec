8af5e26f704fc5ee9538ddd41dc1949e73e40ae6
Gumaro Melendez <cloudernew@gmail.com>
2022-03-25 17:20:12 -0600

    2 Merge nodes spec defs

--- Clauses/MERGE/2 Merge nodes.js
+++ Clauses/MERGE/2 Merge nodes.js
@@ -4,6 +4,24 @@
  * MERGE (robert:Critic)
  * RETURN robert, labels(robert)
  */
+[
+    { clause: {
+        name: "MERGE"
+        ,expression: { pattern: [
+            { node: { name: "robert" ,label: "Critic" } }
+        ] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expressions: [
+            "robert"
+            ,{ function: {
+                name: "labels"
+                ,argument: { expression: "robert" }
+            } }
+        ]
+    } }
+]
 
 /**
  * 2.2. Merge single node with properties
@@ -11,6 +29,24 @@
  * MERGE (charlie {name: 'Charlie Sheen', age: 10})
  * RETURN charlie
  */
+[
+    { clause: {
+        name: "MERGE"
+        ,expression: { pattern: [
+            { node: {
+                name: "charlie"
+                ,properties: {
+                    name: { literal: "Charlie Sheen" }
+                    ,age: { literal: 10 }
+                }
+            } }
+        ] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: "charlie"
+    } }
+]
 
 /**
  * 2.3. Merge single node specifying both label and property
@@ -18,6 +54,33 @@
  * MERGE (michael:Person {name: 'Michael Douglas'})
  * RETURN michael.name, michael.bornIn
  */
+[
+    { clause: {
+        name: "MERGE"
+        ,expression: { pattern: [
+            { node: {
+                name: "michael"
+                ,label: "Person"
+                ,properties: {
+                    name: { literal: "Michael Douglas" }
+                }
+            } }
+        ] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expressions: [
+            { operator: {
+                name: "."
+                ,expressions: ["michael" ,"name"]
+            } }
+            ,{ operator: {
+                name: "."
+                ,expressions: ["michael" ,"bornIn"]
+            } }
+        ]
+    } }
+]
 
 /**
  * 2.4. Merge single node derived from an existing node property
@@ -26,3 +89,22 @@
  * MERGE (city:City {name: person.bornIn})
  * RETURN person.name, person.bornIn, city
  */
+[
+    { clause: {
+        name: "MATCH"
+        ,expression: { pattern: [
+            { node: { name: "person" ,label: "Person"} }
+        ] }
+    } }
+    ,{ clause: {
+        name: "MERGE"
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expressions: [
+            { operator: "." ,expressions: ["person" ,"name"] }
+            ,{ operator: "." ,expression: ["person" ,"bornIn"] }
+            ,"city"
+        ]
+    } }
+]
