9ce1e7740a3678344601bec589b074565449694d
Gumaro Melendez <cloudernew@gmail.com>
2022-03-25 23:05:07 -0600

    REMOVE clause spec defs

--- Clauses/REMOVE/REMOVE.js
+++ Clauses/REMOVE/REMOVE.js
@@ -5,6 +5,28 @@
  * REMOVE a.age
  * RETURN a.name, a.age
  */
+[
+    { clause: {
+        name: "MATCH"
+        ,expression: { pattern: [
+            { node: {
+                name: "a"
+                ,properties: [{ name: {literal: "Andy" } }]
+            } }
+        ] }
+    } }
+    ,{ clause: {
+        name: "REMOVE"
+        ,expression: { operator: { name: "." ,expressions: ["a" ,"age"] } }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expressions: [
+            { operator: { name: "." ,expressions: ["a" ,"name"] } }
+            ,{ operator: { name: "." ,expressions: ["a" ,"age"] } }
+        ]
+    } }
+]
 
 /**
  * 4. Remove a label from a node
@@ -13,6 +35,28 @@
  * REMOVE n:German
  * RETURN n.name, labels(n)
  */
+[
+    { clause: {
+        name: "MATCH"
+        ,expression: { pattern: [
+            { node: {
+                name: "n"
+                ,properties: [{ name: {literal: "Peter" } }]
+            } }
+        ] }
+    } }
+    ,{ clause: {
+        name: "REMOVE"
+        ,expression: { operator: { name: ":" ,expressions: ["n" ,"German"] } }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expressions: [
+            { operator: { name: "." ,expressions: ["n" ,"name"] } }
+            ,{ function: { name: "labels" ,argument: { expression: "n" } } }
+        ]
+    } }
+]
 
 /**
  * 5. Remove multiple labels from a node
@@ -21,3 +65,28 @@
  * REMOVE n:German:Swedish
  * RETURN n.name, labels(n)
  */
+[
+    { clause: {
+        name: "MATCH"
+        ,expression: { pattern: [
+            { node: {
+                name: "n"
+                ,properties: [{ name: {literal: "Peter" } }]
+            } }
+        ] }
+    } }
+    ,{ clause: {
+        name: "REMOVE"
+        ,expression: { operator: {
+            name: ":"
+            ,expressions: ["n" ,"German", "Swedish"]
+        } }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expressions: [
+            { operator: { name: "." ,expressions: ["n" ,"name"] } }
+            ,{ function: { name: "labels" ,argument: { expression: "n" } } }
+        ]
+    } }
+]
