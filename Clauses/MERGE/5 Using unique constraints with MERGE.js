bebf56e50608eda7f25f14d03911eaf9b48bcaf1
Gumaro Melendez <cloudernew@gmail.com>
2022-03-25 20:32:14 -0600

    Merge 5 spec defs

--- Clauses/MERGE/5 Using unique constraints with MERGE.js
+++ Clauses/MERGE/5 Using unique constraints with MERGE.js
@@ -4,6 +4,25 @@
  * MERGE (laurence:Person {name: 'Laurence Fishburne'})
  * RETURN laurence.name
  */
+[
+    { clause: {
+        name: "MERGE"
+        ,expression: { pattern: [
+            { node: {
+                name: "laurence"
+                ,label: "Person"
+                ,properties: { name: { literal: "Laurence Fishburne" } }
+            } }
+        ] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: { operator: {
+            name: "."
+            ,expressions: ["laurence" ,"name"]
+        } }
+    } }
+]
 
 /**
  * 5.2. Merge using unique constraints matches an existing node
@@ -11,6 +30,31 @@
  * MERGE (oliver:Person {name: 'Oliver Stone'})
  * RETURN oliver.name, oliver.bornIn
  */
+[
+    { clause: {
+        name: "MERGE"
+        ,expression: { pattern: [
+            { node: {
+                name: "oliver"
+                ,label: "Person"
+                ,properties: { name: { literal: "Oliver Stone" } }
+            } }
+        ] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expressions: [
+            { operator: {
+                name: "."
+                ,expressions: ["oliver" ,"name"]
+            } }
+            ,{ operator: {
+                name: "."
+                ,expressions: ["oliver" ,"bornIn"]
+            } }
+        ]
+    } }
+]
 
 /**
  * 5.3. Merge with unique constraints and partial matches
@@ -18,6 +62,22 @@
  * MERGE (michael:Person {name: 'Michael Douglas', role: 'Gordon Gekko'})
  * RETURN michael
  */
+[
+    { clause: {
+        name: "MERGE"
+        ,expression: { pattern: [
+            { node: {
+                name: "michael"
+                ,label: "Person"
+                ,properties: {
+                    name: { literal: "Michael Douglas" }
+                    ,role: { literal: "Gordon Geko" }
+                }
+            } }
+        ] }
+    } }
+    ,{ clause: { name: "RETURN" ,expression: "michael" } }
+]
 
 /**
  * 5.4. Merge with unique constraints and conflicting matches
@@ -25,6 +85,22 @@
  * MERGE (oliver:Person {name: 'Oliver Stone', role: 'Gordon Gekko'})
  * RETURN oliver
  */
+[
+    { clause: {
+        name: "MERGE"
+        ,expression: { pattern: [
+            { node: {
+                name: "oliver"
+                ,label: "Person"
+                ,properties: {
+                    name: { literal: "Oliver Stone" }
+                    ,role: { literal: "Gordon Gekko" }
+                }
+            } }
+        ] }
+    } }
+    ,{ clause: { name: "RETURN" ,expression: "oliver" } }
+]
 
 /**
  * 5.5. Using map parameters with MERGE
