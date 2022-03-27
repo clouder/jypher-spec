14e14beefc57fed53c11ffdf25e779fd6724fc29
Gumaro Melendez <cloudernew@gmail.com>
2022-03-26 19:56:19 -0600

    SHOW FUNCTIONS clause spec defs

--- Clauses/SHOW FUNCTIONS/SHOW FUNCTION.js
+++ Clauses/SHOW FUNCTIONS/SHOW FUNCTION.js
@@ -3,6 +3,11 @@
  *
  * SHOW FUNCTIONS
  */
+[
+    { clause: {
+        name: "SHOW FUNCTIONS"
+    } }
+]
 
 /**
  * 3. Listing functions with filtering on output columns
@@ -10,9 +15,30 @@
  * SHOW BUILT IN FUNCTIONS YIELD name, isBuiltIn
  * WHERE name STARTS WITH 'a'
  */
+[
+    { clause: {
+        name: "SHOW FUNCTION"
+        ,modifier: { name: "BUILT IN" }
+        ,yield: { expressions: ["name" ,"isBultIn"] }
+    } }
+    ,{ clause: {
+        name: "WHERE"
+        ,expression: { operator: {
+            name: "STARTS WITH"
+            ,expressions: ["name" ,{ literal: "a" }]
+        } }
+    } }
+]
 
 /**
  * 4. Listing functions with other filtering
  *
  * SHOW FUNCTIONS EXECUTABLE BY CURRENT USER YIELD *
  */
+[
+    { clause: {
+        name: "SHOW FUNCTION"
+        ,modifier: { name: "EXECUTABLE BY CURRENT USER" }
+        ,yield: { expression: "*" }
+    } }
+]
