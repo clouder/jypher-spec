ace4f736c20ff51fe3f21008abc90fe49c138736
Gumaro Melendez <cloudernew@gmail.com>
2022-03-26 20:30:46 -0600

    SKIP clause spec defs

--- Clauses/SKIP/SKIP.js
+++ Clauses/SKIP/SKIP.js
@@ -6,6 +6,20 @@
  * ORDER BY n.name
  * SKIP 3
  */
+[
+    { clause: {
+        name: "MATCH", expression: { pattern: [{ node: { name: "n" } }] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: { operator: { name: "." ,expressions: ["n" ,"name"] } }
+    } }
+    ,{ clause: {
+        name: "ORDER BY"
+        ,expression: { operator: { name: "." ,expressions: ["n" ,"name"] } }
+    } }
+    ,{ clause: { name: "SKIP", expression: { literal: 3 } } }
+]
 
 /**
  * 3. Return middle two rows
@@ -16,6 +30,21 @@
  * SKIP 1
  * LIMIT 2
  */
+[
+    { clause: {
+        name: "MATCH", expression: { pattern: [{ node: { name: "n" } }] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: { operator: { name: "." ,expressions: ["n" ,"name"] } }
+    } }
+    ,{ clause: {
+        name: "ORDER BY"
+        ,expression: { operator: { name: "." ,expressions: ["n" ,"name"] } }
+    } }
+    ,{ clause: { name: "SKIP", expression: { literal: 1 } } }
+    ,{ clause: { name: "LIMIT", expression: { literal: 2 } } }
+]
 
 /**
  * 4. Using an expression with SKIP to return a subset of the rows
@@ -25,3 +54,35 @@
  * ORDER BY n.name
  * SKIP 1 + toInteger(3*rand())
  */
+[
+    { clause: {
+        name: "MATCH", expression: { pattern: [{ node: { name: "n" } }] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: { operator: { name: "." ,expressions: ["n" ,"name"] } }
+    } }
+    ,{ clause: {
+        name: "ORDER BY"
+        ,expression: { operator: { name: "." ,expressions: ["n" ,"name"] } }
+    } }
+    ,{ clause: {
+        name: "SKIP"
+        , expression: { operator: {
+            name: "+"
+            ,expressions: [
+                { literal: 1 }
+                ,{ function: {
+                    name: "toInteger"
+                    ,argument: { operator: {
+                        name: "*"
+                        ,expressions: [
+                            { literal: 3 }
+                            ,{ function: { name: "rand" } }
+                        ]
+                    } }
+                } }
+            ]
+        } }
+    } }
+]
