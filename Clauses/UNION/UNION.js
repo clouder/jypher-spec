4a6283004ee0eea79e040bc6f3c8e48f87dbdf25
Gumaro Melendez <cloudernew@gmail.com>
2022-03-26 20:40:15 -0600

    UNION spec defs

--- Clauses/UNION/UNION.js
+++ Clauses/UNION/UNION.js
@@ -7,6 +7,37 @@
  * MATCH (n:Movie)
  * RETURN n.title AS name
  */
+[
+    { clause: {
+        name: "MATCH"
+        ,expression: { pattern: [{ node: { name: "n" ,label: "Actor" } }] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: { operator: {
+            name: "AS"
+            ,expressions: [
+                { operator: { name: "." ,expressions: ["n" ,"name"] } }
+                ,"name"
+            ]
+        } }
+    } }
+    ,{ clause: { name: "UNION" ,modifier: { name: "ALL" } } }
+    ,{ clause: {
+        name: "MATCH"
+        ,expression: { pattern: [{ node: { name: "n" ,label: "Movie" } }] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: { operator: {
+            name: "AS"
+            ,expressions: [
+                { operator: { name: "." ,expressions: ["n" ,"title"] } }
+                ,"name"
+            ]
+        } }
+    } }
+]
 
 /**
  * 3. Combine two queries and remove duplicates
@@ -17,3 +48,34 @@
  * MATCH (n:Movie)
  * RETURN n.title AS name
  */
+[
+    { clause: {
+        name: "MATCH"
+        ,expression: { pattern: [{ node: { name: "n" ,label: "Actor" } }] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: { operator: {
+            name: "AS"
+            ,expressions: [
+                { operator: { name: "." ,expressions: ["n" ,"name"] } }
+                ,"name"
+            ]
+        } }
+    } }
+    ,{ clause: { name: "UNION" } }
+    ,{ clause: {
+        name: "MATCH"
+        ,expression: { pattern: [{ node: { name: "n" ,label: "Movie" } }] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: { operator: {
+            name: "AS"
+            ,expressions: [
+                { operator: { name: "." ,expressions: ["n" ,"title"] } }
+                ,"name"
+            ]
+        } }
+    } }
+]
