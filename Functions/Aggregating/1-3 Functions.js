02ee574f4f65f2cdcd125c22c80ee27856588d24
Gumaro Melendez <cloudernew@gmail.com>
2022-03-26 23:35:41 -0600

    Aggregating Functions 1-3 spec defs

--- Functions/Aggregating/1-3 Functions.js
+++ Functions/Aggregating/1-3 Functions.js
@@ -4,6 +4,24 @@
  * MATCH (n:Person)
  * RETURN avg(n.age)
  */
+[
+    { clause: {
+        name: "MATCH"
+        ,expression: { pattern: [
+            { node: { name: "n" ,label: "Person" } }
+        ] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: { function: {
+            name: "avg"
+            ,argument: { operator: {
+                name: "."
+                ,expressions: ["n" ,"age"]
+            } }
+        } }
+    } }
+]
 
 /**
  * 2. avg() - Durations
@@ -11,6 +29,34 @@
  * UNWIND [duration('P2DT3H'), duration('PT1H45S')] AS dur
  * RETURN avg(dur)
  */
+[
+    { clause: {
+        name: "UNWIND"
+        ,expression: { operator: {
+            name: "AS"
+            ,expressions: [
+                { literal: [
+                    { function: {
+                        name: "duration"
+                        ,argument: { literal: "P2DT3H"}
+                    } }
+                    ,{ function: {
+                        name: "duration"
+                        ,argument: { literal: "PT1H45S"}
+                    } }
+                ] }
+                ,"dur"
+            ]
+        } }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: { function: {
+            name: "avg"
+            ,argument: { expression: "dur"}
+        } }
+    } }
+]
 
 /**
  * 3. collect()
@@ -18,3 +64,16 @@
  * MATCH (n:Person)
  * RETURN collect(n.age)
  */
+[
+    { clause: {
+        name: "MATCH"
+        ,expression: { pattern: [{ node: { name: "n" ,label: "Person" } }] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: { function: {
+            name: "collect"
+            ,argument: { operator: { name: "." ,expressions: ["n" ,"age"] } }
+        } }
+    } }
+]
