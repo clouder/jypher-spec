a9e25e18a4355525a8d7a749a4afc9b2fb725651
Gumaro Melendez <cloudernew@gmail.com>
2022-03-26 20:10:35 -0600

    SHOW PROCEDURES spec defs

--- Clauses/SHOW PROCEDURES/SHOW PROCEDURES.js
+++ Clauses/SHOW PROCEDURES/SHOW PROCEDURES.js
@@ -3,6 +3,11 @@
  *
  * SHOW PROCEDURES
  */
+[
+    { clause: {
+        name: "SHOW PROCEDURES"
+    } }
+]
 
 /**
  * 3. Listing procedures with filtering on output columns
@@ -10,11 +15,33 @@
  * SHOW PROCEDURES YIELD name, admin
  * WHERE admin
  */
+[
+    { clause: {
+        name: "SHOW PROCEDURES"
+        ,yield: { expressions: ["name" ,"admin"] }
+    } }
+    ,{ clause: { name: "WHERE", expression: "admin" } }
+]
 
 /**
  * 4. Listing procedures with other filtering
  *
  * SHOW PROCEDURES EXECUTABLE BY CURRENT USER YIELD *
- *
+ */
+[
+    { clause: {
+        name: "SHOW PROCEDURES"
+        ,modifier: { name: "EXECUTABLE BY CURRENT USER" }
+        ,yield: { expression: "*" }
+    } }
+]
+
+/**
  * SHOW PROCEDURES EXECUTABLE BY jake
  */
+[
+    { clause: {
+        name: "SHOW PROCEDURES"
+        ,modifier: { name: "EXECUTABLE BY", user: "jake" }
+    } }
+]
