9e3e273b005c316f394d51998da1c35490d0ad83
Gumaro Melendez <cloudernew@gmail.com>
2022-03-26 23:20:13 -0600

    UNWIND clause spec defs

--- Clauses/UNWIND/UNWIND.js
+++ Clauses/UNWIND/UNWIND.js
@@ -4,6 +4,33 @@
  * UNWIND [1, 2, 3, null] AS x
  * RETURN x, 'val' AS y
  */
+[
+    { clause: {
+        name: "UNWIND"
+        ,expression: { operator: {
+            name: "AS"
+            ,expressions: [
+                { literal: [
+                    { literal: 1 }
+                    ,{ literal: 2 }
+                    ,{ literal: 3 }
+                    ,{ literal: null }
+                ] }
+                ,"x"
+            ]
+        } }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expressions: [
+            "x"
+            ,{ operator: {
+                name: "AS"
+                ,expression: [{ literal: "val" } ,"y"]
+            } }
+        ]
+    } }
+]
 
 /**
  * 3. Creating a distinct list
@@ -13,6 +40,27 @@
  * WITH DISTINCT x
  * RETURN collect(x) AS setOfVals
  */
+[
+    { clause: {
+        name: "WITH"
+    } }
+    ,{ clause: {
+        name: "UNWIND"
+    } }
+    ,{ clause: {
+        name: "WITH"
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: { operator: {
+            name: "AS"
+            ,expressions: [
+                { function: { name: "collect" ,argument: { expression: "x" } } }
+                ,"setOfVals"
+            ]
+        } }
+    } }
+]
 
 /**
  * 4. Using UNWIND with any expression returning a list
@@ -23,6 +71,44 @@
  * UNWIND (a + b) AS x
  * RETURN x
  */
+[
+    { clause: {
+        name: "WITH"
+        ,expressions: [
+            { operator: {
+                name: "AS"
+                ,expressions: [
+                    { literal: [{ literal: 1 }, { literal: 2 }]}
+                    ,"a"
+                ]
+            } }
+            ,{ operator: {
+                name: "AS"
+                ,expressions: [
+                    { literal: [{ literal: 3 }, { literal: 4 }]}
+                    ,"b"
+                ]
+            } }
+        ]
+    } }
+    ,{ clause: {
+        name: "UNWIND"
+        ,expression: { operator: {
+            name: "AS"
+            ,expressions: [
+                { operator: {
+                    name: "()"
+                    ,expression: { operator: {
+                        name: "+"
+                        ,expressions: ["a" ,"b"]
+                    } }
+                } }
+                ,"x"
+            ]
+        }}
+    } }
+    ,{ clause: { name: "RETURN" ,expression: "x" } }
+]
 
 /**
  * 5. Using UNWIND with a list of lists
@@ -32,6 +118,31 @@
  * UNWIND x AS y
  * RETURN y
  */
+[
+    { clause: {
+        name: "WITH"
+        ,expression: { operator: {
+            name: "AS"
+            ,expressions: [
+                { literal: [
+                    { literal: [{ literal: 1 }, { literal: 2 }] }
+                    ,{ literal: [{ litera: 3 }, { literal: 4 }] }
+                    ,{ literal: 5 }
+                ] }
+                ,"nested"
+            ]
+        } }
+    } }
+    ,{ clause: {
+        name: "UNWIND"
+        ,expression: { operator: { name: "AS" ,expressions: ["nested", "x"] } }
+    } }
+    ,{ clause: {
+        name: "UNWIND"
+        ,expression: { operator: { name: "AS" ,expressions: ["x" ,"y"] } }
+    } }
+    ,{ clause: { name: "RETURN" ,expression: "y" } }
+]
 
 /**
  * 6. Using UNWIND with an empty list
@@ -39,6 +150,22 @@
  * UNWIND [] AS empty
  * RETURN empty, 'literal_that_is_not_returned'
  */
+[
+    { clause: {
+        name: "UNWIND"
+        ,expression: { operator: {
+            name: "AS"
+            ,expressions: [ { literal: [] } ,"empty"]
+        } }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expressions: [
+            "empty"
+            ,{ literal: "literal_that_is_not_returned" }
+        ]
+    } }
+]
 
 /**
  * 7. Using UNWIND with an expression that is not a list
@@ -46,6 +173,22 @@
  * UNWIND null AS x
  * RETURN x, 'some_literal'
  */
+[
+    { clause: {
+        name: "UNWIND"
+        ,expression: { operator: {
+            name: "AS"
+            ,expressions: ["x" ,{ literal: "some_literal" }]
+        } }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expressions: [
+            "x"
+            ,{ literal: "some_literal" }
+        ]
+    } }
+]
 
 /**
  * [NOTICE]
