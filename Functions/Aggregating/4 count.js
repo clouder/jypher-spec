749f786680d7c6205262d47f6c1246323d212b51
Gumaro Melendez <cloudernew@gmail.com>
2022-03-28 00:45:48 -0600

    Functions Aggreating count spec defs

--- Functions/Aggregating/4 count.js
+++ Functions/Aggregating/4 count.js
@@ -4,6 +4,33 @@
  * MATCH (n {name: 'A'})-->(x)
  * RETURN labels(n), n.age, count(*)
  */
+[
+    { clause: {
+        name: "MATCH"
+        ,expression: { pattern: [
+            { node: {
+                name: "n"
+                ,properties: { name: { literal: "A" } }
+            } }
+            ,{ edge: { direction: ">" } }
+            ,{ node: { name: "x" } }
+        ] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expressions: [
+            { function: { name: "labels" ,argument: { expression: "n" } } }
+            ,{ operator: {
+                name: "."
+                ,expressions: ["n" ,"age"]
+            } }
+            ,{ function: {
+                name: "count"
+                ,argument: { expression: "*" }
+            } }
+        ]
+    } }
+]
 
 /**
  * 4.2. Using count(*) to group and count relationship types
@@ -11,6 +38,29 @@
  * MATCH (n {name: 'A'})-[r]->()
  * RETURN type(r), count(*)
  */
+[
+    { clause: {
+        name: "MATCH"
+        ,expression: { pattern: [
+            { node: { name: "n" ,properties: { name: { literal: "A" } } } }
+            ,{ edge: { name: "r" ,direction: ">" } }
+            ,{ node: {} }
+        ] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expressions: [
+            { function: {
+                name: "type"
+                ,argument: { expression: "r" }
+            } }
+            ,{ function: {
+                name: "count"
+                ,argument: { expression: "*" }
+            } }
+        ]
+    } }
+]
 
 /**
  * 4.3. Using count(expression) to return the number of values
@@ -18,6 +68,28 @@
  * MATCH (n {name: 'A'})-->(x)
  * RETURN count(x)
  */
+[
+    { clause: {
+        name: "MATCH"
+        ,expression: { pattern: [
+            { node: {
+                name: "n"
+                ,properties: {
+                    name: { literal: "A" }
+                }
+            } }
+            ,{ edge: { direction: ">" } }
+            ,{ node: { name: "x" } }
+        ] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: { function: {
+            name: "count"
+            ,argument: { expression: "x" }
+        } }
+    } }
+]
 
 /**
  * 4.4. Counting non-null values
@@ -25,6 +97,24 @@
  * MATCH (n:Person)
  * RETURN count(n.age)
  */
+[
+    { clause: {
+        name: "MATCH"
+        ,expression: { pattern: [
+            { node: { name: "n" ,label: "Person" } }
+        ] }
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expression: { function: {
+            name: "count"
+            ,argument: { operator: {
+                name: "."
+                ,expressions: ["n" ,"age"]
+            } }
+        } }
+    } }
+]
 
 /**
  * 4.5. Counting with and without duplicates
@@ -33,3 +123,27 @@
  * WHERE me.name = 'A'
  * RETURN count(DISTINCT friend_of_friend), count(friend_of_friend)
  */
+[
+    { clause: {
+        name: "MATCH"
+    } }
+    ,{ clause: {
+        name: "WHERE"
+    } }
+    ,{ clause: {
+        name: "RETURN"
+        ,expressions: [
+            { function: {
+                name: "count"
+                ,expression: { operator: {
+                    name: "DISTINCT"
+                    ,expression: "friend_of_friend"
+                } }
+            } }
+            ,{ function: {
+                name: "count"
+                ,argument: { expression: "friend_of_friend" }
+            } }
+        ]
+    } }
+]
