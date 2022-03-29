2bd1fc43c3632af644f1ace9046ca152545ec36b
Gumaro Melendez <cloudernew@gmail.com>
2022-03-29 11:34:38 -0600

    Logarithmic Functions spec defs

--- Functions/Mathematical/Logarithmic.js
+++ Functions/Mathematical/Logarithmic.js
@@ -3,27 +3,57 @@
  *
  * RETURN e()
  */
+[
+    { clause: {
+        name: "RETURN"
+        ,expression: { function: { name: "e" } }
+    } }
+]
 
 /**
  * 2. exp()
  *
  * RETURN exp(2)
  */
+[
+    { clause: {
+        name: "RETURN"
+        ,expression: { function: { name: "exp" ,argument: { literal: 2 } } }
+    } }
+]
 
 /**
  * 3. log()
  *
  * RETURN log(27)
  */
+ [
+    { clause: {
+        name: "RETURN"
+        ,expression: { function: { name: "log" ,argument: { literal: 27 } } }
+    } }
+]
 
 /**
  * 4. log10()
  *
  * RETURN log10(27)
  */
+ [
+    { clause: {
+        name: "RETURN"
+        ,expression: { function: { name: "log10" ,argument: { literal: 27 } } }
+    } }
+]
 
 /**
  * 5. sqrt()
  *
  * RETURN sqrt(256)
  */
+ [
+    { clause: {
+        name: "RETURN"
+        ,expression: { function: { name: "sqrt" ,argument: { literal: 256 } } }
+    } }
+]
